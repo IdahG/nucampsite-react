e5c3740354f9a6f73b59bcbaa5c80a0fdcdb7d3d
Saidah <saeeda_g@hotmail.com>
2023-10-28 16:29:25 +0800

    validateContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,7 @@
 import {Button, Label, Col , FormGroup } from 'reactstrap';
-import {Formik, Field, Form } from 'formik';
+import {Formik, Field, Form , ErrorMessage} from 'formik';
+import validateContactForm from '../utils/validateContactForm';
+
 
 const ContactForm = () => {
     const handleSubmit = (values, { resetForm }) => {
@@ -20,6 +22,7 @@ const ContactForm = () => {
             
             }}
             onSubmit={handleSubmit}
+            validate={validateContactForm}
             
             >
                 <Form>
@@ -27,28 +30,45 @@ const ContactForm = () => {
                     <Label htmlFor='firstName' md='2'>
                         First Name
                     </Label>
-                    <Col md='10'> <Field className='form-control'name ='firstName' placeholder='First Name'/>
+                    <Col md='10'>
+                         <Field className='form-control'name ='firstName' placeholder='First Name'/>
+                         <ErrorMessage name='firstName'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
+
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label htmlFor='lastName' md='2'>
                         Last Name
                     </Label>
-                    <Col md='10'> <Field className='form-control'name ='lastName' placeholder='Last Name'/>
+                    <Col md='10'> 
+                    <Field className='form-control'name ='lastName' placeholder='Last Name'/>
+                    <ErrorMessage name='lastName'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label htmlFor='phoneNum' md='2'>
                         Phone
                     </Label>
-                    <Col md='10'> <Field className='form-control'name ='phoneNum' placeholder='Phone'/>
+                    <Col md='10'>
+                         <Field className='form-control'name ='phoneNum' placeholder='Phone'/>
+                         <ErrorMessage name='phoneNum'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
                     <Label htmlFor='email' md='2'>
                         Email
                     </Label>
-                    <Col md='10'> <Field className='form-control'name ='email' placeholder='Email'/>
+                    <Col md='10'> 
+                    <Field className='form-control'name ='email' placeholder='Email'/>
+                    <ErrorMessage name='email'>
+                            {(msg) => <p className='text-danger'>{msg}</p>}
+                        </ErrorMessage>
                     </Col>
                 </FormGroup>
                 <FormGroup row>
